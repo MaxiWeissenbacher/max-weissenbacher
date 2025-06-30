8857206512140995f3cd121c5be48276474ab491
Maximilian Weißenbacher (DE) <maximilian.weissenbacher@pwc.com>
2025-06-30 15:47:44 +0200

    Fix routing for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/max-weissenbacher/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
